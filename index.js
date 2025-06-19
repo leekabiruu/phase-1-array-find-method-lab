e08ee6ea7022cd4a6d7f47b8d4ca15f43e89af81
Lee <leekinyuakabiru@gmail.com>
2025-06-19 14:51:04 +0300

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // code your solution here
+const record = [
+    {
+        year: 2016,results: "Winners",
+        year: 2017,results: "N/A",
+        year: 2018,results: "Losers",
+    }
+]
+
+function superbowlWin(record) {
+  const win = record.find(obj => obj.result === "W");
+
+  if (win) {
+    return win.year;
+  } else {
+    return undefined;
+  }
+ 
+}
+ console.log(superbowlWin(record))
